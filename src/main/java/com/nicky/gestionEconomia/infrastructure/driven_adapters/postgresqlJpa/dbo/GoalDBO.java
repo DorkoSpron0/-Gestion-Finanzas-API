8fa82d0068a59905ddb63e29073b4ed05a8ad1c1
dorkospron0 <tareassec9@gmail.com>
2025-06-17 12:57:46 -0500

    GoalDBO fromDomain and toDomain

--- src/main/java/com/nicky/gestionEconomia/infrastructure/driven_adapters/postgresqlJpa/dbo/GoalDBO.java
+++ src/main/java/com/nicky/gestionEconomia/infrastructure/driven_adapters/postgresqlJpa/dbo/GoalDBO.java
@@ -1,8 +1,7 @@
 package com.nicky.gestionEconomia.infrastructure.driven_adapters.postgresqlJpa.dbo;
 
 
-import com.nicky.gestionEconomia.domain.models.GoalState;
-import com.nicky.gestionEconomia.domain.models.UserDomain;
+import com.nicky.gestionEconomia.domain.models.*;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,4 +36,88 @@ public class GoalDBO {
 
     @OneToMany(mappedBy = "goal", cascade = CascadeType.ALL)
     private List<TransactionDBO> transactions;
+
+    public static GoalDBO fromDomain(GoalDomain domain){
+        GoalDBO.GoalDBOBuilder builder = GoalDBO.builder()
+                .id(domain.id())
+                .name(domain.name())
+                .goalAmount(domain.goalAmount())
+                .dueDate(domain.dueDate())
+                .state(domain.state());
+
+        if(domain.user() != null){
+            builder.user(UserDBO.builder().id(domain.user().id()).build());
+        }
+
+        if(domain.transactions() != null){
+            builder.transactions(
+                    domain.transactions().stream()
+                            .map(transactionDomain -> TransactionDBO.builder()
+                                    .id(transactionDomain.id())
+                                    .date(transactionDomain.date())
+                                    .amount(transactionDomain.amount())
+                                    .description(transactionDomain.description())
+                                    .build())
+                            .toList()
+            );
+        }
+
+        return builder.build();
+    }
+
+    public GoalDomain toDomain(){
+        return new GoalDomain(
+                id,
+                name,
+                getGoalAmount(),
+                dueDate,
+                state,
+                user != null ? new UserDomain(
+                        user.getId(),
+                        user.getName(),
+                        user.getEmail(),
+                        user.getPassword(),
+                        user.getDate(),
+                        null,
+                        null,
+                        null,
+                        null
+                ) : null,
+                transactions != null ? transactions.stream()
+                        .map(transactionDBO -> new TransactionDomain(
+                                transactionDBO.getId(),
+                                transactionDBO.getDate(),
+                                transactionDBO.getAmount(),
+                                transactionDBO.getDescription(),
+                                transactionDBO.getUser() != null ? new UserDomain(
+                                        transactionDBO.getUser().getId(),
+                                        transactionDBO.getUser().getName(),
+                                        transactionDBO.getUser().getEmail(),
+                                        transactionDBO.getUser().getPassword(),
+                                        transactionDBO.getUser().getDate(),
+                                        null,
+                                        null,
+                                        null,
+                                        null
+                                ) : null,
+                                transactionDBO.getCategory() != null ? new CategoryDomain(
+                                        transactionDBO.getCategory().getId(),
+                                        transactionDBO.getCategory().getName(),
+                                        transactionDBO.getCategory().getType(),
+                                        null,
+                                        null
+                                ) : null,
+                                transactionDBO.getAccount() != null ? new AccountDomain(
+                                        transactionDBO.getAccount().getId(),
+                                        transactionDBO.getAccount().getName(),
+                                        transactionDBO.getAccount().getType(),
+                                        transactionDBO.getAccount().getCurrentAmount(),
+                                        transactionDBO.getAccount().getActive(),
+                                        null,
+                                        null
+                                ) : null,
+                                null
+                        )).toList() : null
+        );
+    }
 }
