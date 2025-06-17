6c6480ecd6d60d26ef9ce9f2c948919abfa57619
dorkospron0 <tareassec9@gmail.com>
2025-06-17 13:08:10 -0500

    CategoryDBO fromDomain and toDomain

--- src/main/java/com/nicky/gestionEconomia/infrastructure/driven_adapters/postgresqlJpa/dbo/CategoryDBO.java
+++ src/main/java/com/nicky/gestionEconomia/infrastructure/driven_adapters/postgresqlJpa/dbo/CategoryDBO.java
@@ -1,7 +1,6 @@
 package com.nicky.gestionEconomia.infrastructure.driven_adapters.postgresqlJpa.dbo;
 
-import com.nicky.gestionEconomia.domain.models.TransactionDomain;
-import com.nicky.gestionEconomia.domain.models.UserDomain;
+import com.nicky.gestionEconomia.domain.models.*;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,4 +30,97 @@ public class CategoryDBO {
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<TransactionDBO> transactions;
+
+    public static CategoryDBO fromDomain(CategoryDomain domain){
+        CategoryDBO.CategoryDBOBuilder builder = CategoryDBO.builder()
+                .id(domain.id())
+                .name(domain.name())
+                .type(domain.type());
+
+        if(domain.user() != null) {
+            builder.user(
+                    new UserDBO(domain.user().id(), null, null, null, null, null, null, null, null)
+            );
+        }
+
+        if(domain.transactions() != null){
+            builder.transactions(
+                    domain.transactions().stream()
+                            .map(transactionDomain -> new TransactionDBO(
+                                    transactionDomain.id(),
+                                    null,
+                                    null,
+                                    null,
+                                    null,
+                                    null,
+                                    null,
+                                    null
+                            )).toList()
+            );
+        }
+
+        return builder.build();
+    }
+
+    public CategoryDomain toDomain(){
+        return new CategoryDomain(
+                id,
+                name,
+                type,
+                user != null ? new UserDomain(
+                        user.getId(),
+                        user.getName(),
+                        user.getEmail(),
+                        user.getPassword(),
+                        user.getDate(),
+                        null,
+                        null,
+                        null,
+                        null
+                ) : null,
+                transactions != null ? transactions.stream()
+                        .map(transactionDBO -> new TransactionDomain(
+                                transactionDBO.getId(),
+                                transactionDBO.getDate(),
+                                transactionDBO.getAmount(),
+                                transactionDBO.getDescription(),
+                                transactionDBO.getUser() != null ? new UserDomain(
+                                        transactionDBO.getUser().getId(),
+                                        transactionDBO.getUser().getName(),
+                                        transactionDBO.getUser().getEmail(),
+                                        transactionDBO.getUser().getPassword(),
+                                        transactionDBO.getUser().getDate(),
+                                        null,
+                                        null,
+                                        null,
+                                        null
+                                ) : null,
+                                transactionDBO.getCategory() != null ? new CategoryDomain(
+                                        transactionDBO.getCategory().getId(),
+                                        transactionDBO.getCategory().getName(),
+                                        transactionDBO.getCategory().getType(),
+                                        null,
+                                        null
+                                ) : null,
+                                transactionDBO.getAccount() != null ? new AccountDomain(
+                                        transactionDBO.getAccount().getId(),
+                                        transactionDBO.getAccount().getName(),
+                                        transactionDBO.getAccount().getType(),
+                                        transactionDBO.getAccount().getCurrentAmount(),
+                                        transactionDBO.getAccount().getActive(),
+                                        null,
+                                        null
+                                ) : null,
+                                transactionDBO.getGoal() != null ? new GoalDomain(
+                                        transactionDBO.getGoal().getId(),
+                                        transactionDBO.getGoal().getName(),
+                                        transactionDBO.getGoal().getGoalAmount(),
+                                        transactionDBO.getGoal().getDueDate(),
+                                        transactionDBO.getGoal().getState(),
+                                        null,
+                                        null
+                                ) : null
+                        )).toList() : null
+        );
+    }
 }
